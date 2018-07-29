87974d52c5eec65f9537ba8f484407bc5464ee69
Niklas Werner <Niklas-Werner@outlook.com>
2018-07-29 15:24:48 +0200

    added DateUtils

---
+++ src/NW55.Core/Extensions/DateUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NW55.Extensions
+{
+    // https://stackoverflow.com/questions/1004698
+
+    public static class DateUtils
+    {
+        public static DateTime Truncate(this DateTime date, long ticksResolution)
+            => new DateTime(date.Ticks - (date.Ticks % ticksResolution), date.Kind);
+
+        public static DateTime ToSecondsResolution(this DateTime date)
+            => date.Truncate(TimeSpan.TicksPerSecond);
+
+        public static DateTime UtcNowSeconds => DateTime.UtcNow.ToSecondsResolution();
+    }
+}
